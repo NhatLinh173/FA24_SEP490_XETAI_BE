dd7fa9eb72d68c5fe4206181238debf4952de6f1
quang <quanggaming2002@gmail.com>
2024-10-03 14:53:43 +0700

    update lai

--- src/router/search.js
+++ src/router/search.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router=express.Router();
 const searchController = require('../controller/SearchController');
 
-router.get("/:keyword",searchController.search);
+router.get("/",searchController.searchByStartPointAndDestination); //http://localhost:3006/search?startPoint=Hanoi&destination=Saigon
 
 module.exports = router;
