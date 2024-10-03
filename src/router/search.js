387530b02518f125966f8b0215dfc49d5c4643a1
Nguyễn Minh Quang <97968904+IceVision076@users.noreply.github.com>
2024-10-03 14:40:51 +0700

    Update search.js

--- src/router/search.js
+++ src/router/search.js
@@ -3,6 +3,6 @@ const router=express.Router();
 const searchController = require('../controller/SearchController');
 
 //router.get("/:keyword",searchController.search);
-router.get("/",searchByStartPointAndDestination.search); //http://localhost:3006/search?startPoint=Hanoi&destination=Saigon
+router.get("/",searchController.searchByStartPointAndDestination); //http://localhost:3006/search?startPoint=Hanoi&destination=Saigon
 
 module.exports = router;
