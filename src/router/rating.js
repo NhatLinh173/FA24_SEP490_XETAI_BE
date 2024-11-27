a74f8d73b1892da69b40e92b43448b27b45a37af
linh <linhnvnde160328@fpt.edu.vn>
2024-11-27 14:35:19 +0700

    Fix Favorite driver && Update Status Post

--- src/router/rating.js
+++ src/router/rating.js
@@ -1,18 +1,21 @@
-const express = require('express');
-const router=express.Router();
-const RatingController = require('../controller/RatingController');
+const express = require("express");
+const router = express.Router();
+const RatingController = require("../controller/RatingController");
 
-router.post("/:postId",RatingController.addRating);
+router.post("/:postId", RatingController.addRating);
 
-router.put("/",RatingController.updateRating);
+router.put("/", RatingController.updateRating);
 
 // Lấy danh sách đánh giá của bản thân (do người khác đánh giá)
-router.get('/my-ratings/:userId', RatingController.getMyRatings);
-
+router.get("/my-ratings/:userId", RatingController.getMyRatings);
+router.get("/ratings/post/:postId", RatingController.getRatingsByPost);
 // Lấy danh sách mình đã đánh giá người khác
-router.get('/ratings-given/:reviewerId', RatingController.getRatingsGivenByUser);
+router.get(
+  "/ratings-given/:reviewerId",
+  RatingController.getRatingsGivenByUser
+);
 
 // lấy ra rating mình đánh giá chính xác tài xế đó
-router.get('/rating-driver', RatingController.getRatingByReviewerForUser);
+router.get("/rating-driver", RatingController.getRatingByReviewerForUser);
 
 module.exports = router;
