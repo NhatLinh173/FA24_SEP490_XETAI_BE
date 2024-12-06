4fe47d96df79080e7c959a6f72dde5eba453d0b1
linh <linhnvnde160328@fpt.edu.vn>
2024-12-06 15:29:59 +0700

    Fix CORS 2

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ const io = socketIO(server, {
       "https://fa-24-sep-490-xetai-fe-jvy4.vercel.app",
       "http://localhost:3000",
       "https://www.xehang.online",
+      "https://xehang.online",
     ],
     methods: ["GET", "POST", "PUT", "PATCH"],
     credentials: true,
